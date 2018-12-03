e06baa39258a93c827f7d0b4f565b8ff7032e15d
0dysseas <kaziolas.od@gmail.com>
2018-12-03 23:28:49 +0200

    cli arg parser done

--- python-east/py_East.py
+++ python-east/py_East.py
@@ -5,27 +5,35 @@ import cv2 as cv
 def parse_cli_arguments():
   parser = argparse.ArgumentParser(description='Tensorflow implementation of EAST: An efficient and Accurate Scene Text Detector')
 
-  parser.add_argument('i', help='| Path to input image or video file. Skip this argument to capture frames from a camera.')
-  parser.add_argument('m', help='| Path to a binary .pb file contains trained network.')
-  parser.add_argument('width', help=' | 320 | Preprocess input image by resizing to a specific width. It should be multiple by 32.')
-  parser.add_argument('height', help='| 320 | Preprocess input image by resizing to a specific height. It should be multiple by 32.')
-  parser.add_argument('thr', help='| 0.5 | Confidence threshold.')
-  parser.add_argument('nms', help='| 0.4 | Non-maximum suppression threshold.')
+  parser.add_argument('-i', help='| Path to input image or video file. Skip this argument to capture frames from a camera.',
+                      type=str)
+  parser.add_argument('-model', help='| Path to a binary .pb file contains trained network.',
+                      type=str)
+  parser.add_argument('-width', help=' | 320 | Preprocess input image by resizing to a specific width. It should be multiple by 32.',
+                      type=int)
+  parser.add_argument('-height', help='| 320 | Preprocess input image by resizing to a specific height. It should be multiple by 32.',
+                      type=int)
+  parser.add_argument('-thr', help='| 0.5 | Confidence threshold.',
+                      type=float)
+  parser.add_argument('-nms', help='| 0.4 | Non-maximum suppression threshold.',
+                      type=float)
   
+  # Show help message and exit if no arguments are passed
   if len(sys.argv) == 1:
     parser.print_help()
     parser.exit()
 
   args = parser.parse_args()
 
+  return args.thr, args.nms, args.width, args.height, args.model
+
 
 
 
 if __name__ == '__main__':
   # Parse command line arguments
-  parse_cli_arguments()
-
-
+  conf_threshold, nms_threshold, width, height, model = parse_cli_arguments()
+  
 
 
 
